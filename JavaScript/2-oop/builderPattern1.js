f117f0c260c58d90c86b2a7e643e342ecd980856
Mohak-Trivedi <trivedimohak@gmail.com>
2024-09-07 19:38:27 +0530

    builder pattern level 1

---
+++ JavaScript/2-oop/builderPattern1.js
@@ -0,0 +1,35 @@
+class Product {
+  constructor(builder) {
+    this.name = builder.name;
+    if (builder.price > 0 && typeof builder.price === "number") {
+      this.price = builder.price;
+    } else {
+      return {};
+    }
+    this.category = builder.category;
+    this.description = builder.description;
+    this.rating = builder.rating;
+  }
+
+  get price() {
+    return this.price;
+  }
+
+  set price(p) {
+    if (p > 0) {
+      this.price = p;
+    } else {
+      console.log("Invalid Price");
+    }
+  }
+}
+
+const p = new Product({
+  name: "iPhone",
+  price: -1000,
+  category: "Electronics",
+  description: "Apple iPhone",
+  rating: 4.5,
+});
+
+console.log(p);
