f3f5149a402717ac47c30427e3defc02412ce134
Mohak-Trivedi <trivedimohak@gmail.com>
2024-10-06 14:08:40 +0530

    performace demo - moved modal open states from App to ButtonWithModal to improve perf

--- ReactJS/performance-demo/src/App.jsx
+++ ReactJS/performance-demo/src/App.jsx
@@ -1,15 +1,12 @@
 import "./App.css";
-import Modal from "./Modal";
+import ButtonWithModal from "./ButtonWithModal";
 import SlowComponent from "./SlowComponent";
-import { useState } from "react";
 
 function App() {
-  const [isOpen, setIsOpen] = useState(false);
   return (
     <>
-      <button onClick={() => setIsOpen(true)}>Open Modal</button>
       <div>Something Done Here</div>
-      {isOpen && <Modal setIsOpen={setIsOpen} />}
+      <ButtonWithModal />
       <div>Something Done Here</div>
       <SlowComponent />
     </>
---
+++ ReactJS/performance-demo/src/ButtonWithModal.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import Modal from "./Modal";
+
+export default function ButtonWithModal() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      <button onClick={() => setIsOpen(true)}>Open Modal</button>
+      {isOpen && <Modal setIsOpen={setIsOpen} />}
+    </>
+  );
+}
