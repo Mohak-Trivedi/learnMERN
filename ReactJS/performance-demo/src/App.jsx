c14540542d410c6eee9816ea28c3681bee1dc95b
Mohak-Trivedi <trivedimohak@gmail.com>
2024-10-06 16:47:38 +0530

    performace-demo used useModalDialogue in ButtonWithModal

--- ReactJS/performance-demo/src/App.jsx
+++ ReactJS/performance-demo/src/App.jsx
@@ -1,16 +1,12 @@
 import "./App.css";
-import useModalDialogue from "./hooks/useModalDialogue";
-import Modal from "./Modal";
+import ButtonWithModal from "./ButtonWithModal";
 import SlowComponent from "./SlowComponent";
-import { useState } from "react";
 
 function App() {
-  const { isOpen, open, close } = useModalDialogue();
   return (
     <>
       <div>Something Done Here</div>
-      <button onClick={open}>Open Modal</button>
-      {isOpen && <Modal close={close} />}
+      <ButtonWithModal />
       <div>Something Done Here</div>
       <SlowComponent />
     </>
--- ReactJS/performance-demo/src/ButtonWithModal.jsx
+++ ReactJS/performance-demo/src/ButtonWithModal.jsx
@@ -1,13 +1,13 @@
-import { useState } from "react";
 import Modal from "./Modal";
+import useModalDialogue from "./hooks/useModalDialogue";
 
 export default function ButtonWithModal() {
-  const [isOpen, setIsOpen] = useState(false);
+  const { isOpen, open, close } = useModalDialogue();
 
   return (
     <>
-      <button onClick={() => setIsOpen(true)}>Open Modal</button>
-      {isOpen && <Modal setIsOpen={setIsOpen} />}
+      <button onClick={open}>Open Modal</button>
+      {isOpen && <Modal close={close} />}
     </>
   );
 }
