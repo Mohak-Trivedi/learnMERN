a7d48e8538a6e66ad58fb1dab9241788d430b3eb
Mohak-Trivedi <trivedimohak@gmail.com>
2024-10-06 13:43:57 +0530

    performance-demo modal

--- ReactJS/performance-demo/src/App.jsx
+++ ReactJS/performance-demo/src/App.jsx
@@ -1,10 +1,15 @@
 import "./App.css";
+import Modal from "./Modal";
 import SlowComponent from "./SlowComponent";
+import { useState } from "react";
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <>
+      <button onClick={() => setIsOpen(true)}>Open Modal</button>
       <div>Something Done Here</div>
+      {isOpen && <Modal setIsOpen={setIsOpen} />}
       <div>Something Done Here</div>
       <SlowComponent />
     </>
