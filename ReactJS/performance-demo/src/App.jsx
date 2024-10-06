dbf8a5b63d0e6af2341fba49dcefa12db543609b
Mohak-Trivedi <trivedimohak@gmail.com>
2024-10-06 18:10:59 +0530

    performance-demo state strictly in App performance issue mimic

--- ReactJS/performance-demo/src/App.jsx
+++ ReactJS/performance-demo/src/App.jsx
@@ -1,14 +1,18 @@
 import "./App.css";
 import ButtonWithModal from "./ButtonWithModal";
 import SlowComponent from "./SlowComponent";
+import { useState } from "react";
 
 function App() {
+  const [x, setX] = useState(0);
   return (
     <>
+      <button onClick={() => setX(x + 1)}>Increment x</button>
       <div>Something Done Here</div>
       <ButtonWithModal />
       <div>Something Done Here</div>
       <SlowComponent />
+      {x}
     </>
   );
 }
