1c4b932d4fb3c763b65650fcad8083ec1cfe3718
Mohak-Trivedi <trivedimohak@gmail.com>
2024-10-06 13:06:57 +0530

    performance demo slow component created

---
+++ ReactJS/performance-demo/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import SlowComponent from "./SlowComponent";
+
+function App() {
+  return (
+    <>
+      <div>Something Done Here</div>
+      <div>Something Done Here</div>
+      <SlowComponent />
+    </>
+  );
+}
+
+export default App;
---
+++ ReactJS/performance-demo/src/SlowComponent.jsx
@@ -0,0 +1,15 @@
+// Note : This Component is slow to render.
+
+const waitingForSomething = (ms) => {
+  const start = Date.now();
+  let now = start;
+
+  while (now - start < ms) {
+    now = Date.now();
+  }
+};
+
+export default function SlowComponent() {
+  waitingForSomething(1000);
+  return null;
+}
