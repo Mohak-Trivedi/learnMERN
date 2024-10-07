0a19018deb70961e6cf0be8374c657bb5329d2fb
Mohak-Trivedi <trivedimohak@gmail.com>
2024-10-07 22:35:00 +0530

    performance demo - children prop to RefactorComponent

--- ReactJS/performance-demo/src/App.jsx
+++ ReactJS/performance-demo/src/App.jsx
@@ -1,18 +1,19 @@
 import "./App.css";
 import ButtonWithModal from "./ButtonWithModal";
 import SlowComponent from "./SlowComponent";
-import { useState } from "react";
+import RefactorComponent from "./RefactorComponent";
 
 function App() {
-  const [x, setX] = useState(0);
   return (
     <>
-      <button onClick={() => setX(x + 1)}>Increment x</button>
-      <div>Something Done Here</div>
-      <ButtonWithModal />
-      <div>Something Done Here</div>
-      <SlowComponent />
-      {x}
+      <RefactorComponent>
+        <>
+          <div>Something Done Here</div>
+          <ButtonWithModal />
+          <div>Something Done Here</div>
+          <SlowComponent />
+        </>
+      </RefactorComponent>
     </>
   );
 }
