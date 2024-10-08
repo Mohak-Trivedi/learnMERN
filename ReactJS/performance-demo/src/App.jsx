434ae83a6c577652301e7c989166bda3d795ffca
Mohak-Trivedi <trivedimohak@gmail.com>
2024-10-08 19:08:53 +0530

    performance demo - function as non-primitive prop to memoized component

--- ReactJS/performance-demo/src/App.jsx
+++ ReactJS/performance-demo/src/App.jsx
@@ -3,8 +3,11 @@ import SlowComponent from "./SlowComponent";
 import Modal from "./Modal";
 import { useState, memo } from "react";
 
-const MemoizedSlowComponent = memo(function ModifiedSlowComponent({ time }) {
-  return <SlowComponent time={time} />;
+const MemoizedSlowComponent = memo(function ModifiedSlowComponent({
+  time,
+  custom,
+}) {
+  return <SlowComponent time={time} custom={custom} />;
 });
 
 function App() {
@@ -15,7 +18,7 @@ function App() {
       {isOpen && <Modal close={() => setIsOpen(false)} />}
       <div>Something Done Here</div>
       <div>Something Done Here</div>
-      <MemoizedSlowComponent time={[1000]} />
+      <MemoizedSlowComponent time={1000} custom={() => {}} />
     </>
   );
 }
