a1ed12c9d8419513f8a1fcb6a9e74dc70f0a6865
Mohak-Trivedi <trivedimohak@gmail.com>
2024-10-08 17:20:03 +0530

    performance demo - React.memo

--- ReactJS/performance-demo/src/App.jsx
+++ ReactJS/performance-demo/src/App.jsx
@@ -1,7 +1,9 @@
 import "./App.css";
 import SlowComponent from "./SlowComponent";
 import Modal from "./Modal";
-import { useState } from "react";
+import { useState, memo } from "react";
+
+const MemoizedSlowComponent = memo(SlowComponent);
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
@@ -11,7 +13,7 @@ function App() {
       {isOpen && <Modal close={() => setIsOpen(false)} />}
       <div>Something Done Here</div>
       <div>Something Done Here</div>
-      <SlowComponent />
+      <MemoizedSlowComponent />
     </>
   );
 }
