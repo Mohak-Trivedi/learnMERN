8d354f9f00f7a02c573138e2f9c086ca96c3d0f4
Mohak-Trivedi <trivedimohak@gmail.com>
2024-10-16 19:56:19 +0530

    mjs vs cjs

--- NodeJS/NodeJSConcepts/globals.js
+++ NodeJS/NodeJSConcepts/globals.js
@@ -13,6 +13,15 @@ console.log(module);
 // modules. It uses the module object to be able to expose variables, functions
 // or classes from a file to other files.
 // There are 2 moduling systems available: ES6 Moduling (New) and CommonJS Moduling (Legacy)
+// Note: You can't have a ES6 moduling file in CommonJS moduling folder, but
+// you can have a file following CommonJS in a ES6 Moduling project by mentioning
+// .cjs extension for that one file, but you can't import this file anywhere.
+// Also, some globals don't work in ES6 Moduling. e.g.from above, only process
+// works in ES6 Modules.
+// CommonJS is enabled by default.
+// To enable ESM, you may use on of the below 3 ways:
+// - .mjs file extensions
+// - in package.json, for type property mention 'module' value.
 
 // require: require function. Used for getting the variable, class or function
 // that we exposed using the module from another files.
