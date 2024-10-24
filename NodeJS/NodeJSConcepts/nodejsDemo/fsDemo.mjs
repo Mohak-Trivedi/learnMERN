e690fb623355e41b87e69ecca96c2bf0338c74e3
Mohak-Trivedi <trivedimohak@gmail.com>
2024-10-24 22:50:44 +0530

    handlebars script using fs

--- NodeJS/NodeJSConcepts/nodejsDemo/fsDemo.mjs
+++ NodeJS/NodeJSConcepts/nodejsDemo/fsDemo.mjs
@@ -1,10 +1,20 @@
-import { readFile } from "fs/promises";
+import { readFile, writeFile } from "fs/promises";
 import { resolve } from "path";
 
 async function readingHtml() {
   const path = resolve("./index.html");
-  const file = await readFile(path);
-  console.log(file.toString());
+  let file = await readFile(path);
+
+  const data = {
+    name: "Mohak",
+    company: "Quizizz",
+  };
+
+  for (const [key, value] of Object.entries(data)) {
+    file = file.toString().replace(`{{${key}}}`, value);
+  }
+
+  await writeFile(resolve("./output.html"), file);
 }
 
 readingHtml();
