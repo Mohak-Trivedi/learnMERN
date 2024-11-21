0b2299859f6044686aa78621e5f421ddb72e47fb
Mohak-Trivedi <trivedimohak@gmail.com>
2024-11-21 23:03:05 +0530

    ping

--- ExpressJS/twitterApp/src/index.js
+++ ExpressJS/twitterApp/src/index.js
@@ -3,6 +3,12 @@ import express from "express";
 // Create a new express app/server object
 const app = express();
 
+app.get("/ping", (req, res) => {
+  return res.json({
+    message: "pong",
+  }); // return this upon GET ping/ request
+});
+
 // Define a port and attach it to the Express app
 app.listen(3000, () => {
   console.log("Server is running on port 3000.");
